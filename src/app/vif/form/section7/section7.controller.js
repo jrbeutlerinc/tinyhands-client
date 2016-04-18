74f127ccbfeb5527e46934ad786e9758235aa280
John Luscombe <johnluscombe@johns-mbp.campus.tayloru.edu>
2016-04-18 17:46:32 -0400

    Started flags

--- src/app/vif/form/section7/section7.controller.js
+++ src/app/vif/form/section7/section7.controller.js
@@ -2,5 +2,15 @@ export default class VifSection7Controller {
   constructor(VifBuilder) {
     'ngInject'
     this.vif = VifBuilder;
+    this.alarms = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.totalAlarms = 0;
+  }
+
+  calculateAlarms() {
+    console.log(this.alarms);
+//    this.totalAlarms = 0;
+//    for (var i = 0; i < this.alarms.length; i++) {
+//        this.totalAlarms += alarms[i];
+//    }
   }
 }
