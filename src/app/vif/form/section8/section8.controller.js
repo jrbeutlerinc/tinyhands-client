91d7292c366a304cf4bcb2d0413783987dd24ee7
jluscomb <jluscomb@brainerd.cs.local>
2016-04-18 23:12:49 -0400

    Static form completed for page 8

--- src/app/vif/form/section8/section8.controller.js
+++ src/app/vif/form/section8/section8.controller.js
@@ -2,5 +2,10 @@ export default class VifSection8Controller {
   constructor(VifBuilder) {
     'ngInject'
     this.vif = VifBuilder;
+    this.sect2Options = ["No trafficking suspected", "Police say not enough information", "Trafficker is victim's own people",
+      "She was going herself", "Trafficker ran away", "Victim afraid of reputation", "Victim afraid for her safety",
+      "Family afraid of reputation", "Family afraid for safety", "Police bribed by trafficker", "Victim / family bribed by trafficker"];
+    this.sect3Options = ["Plans to send the girl home to stay with her guardians", "Plan to send the girl to stay with other relatives",
+      "Tiny Hands needs to help her find another place to go", "Contact THN's Aftercare Coordinator"];
   }
 }
