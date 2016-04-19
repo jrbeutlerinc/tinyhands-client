c8ecfed49995d87bdd345ebdc66d6e47793d65ae
jluscomb <jluscomb@brainerd.cs.local>
2016-04-18 21:52:39 -0400

    Made HTML more angular with ng-repeats

--- src/app/vif/form/section5/section5.controller.js
+++ src/app/vif/form/section5/section5.controller.js
@@ -2,5 +2,6 @@ export default class VifSection5Controller {
   constructor(VifBuilder) {
     'ngInject'
     this.vif = VifBuilder;
+    this.sect1Options = ["Yes", "No", "Meeting Broker", "Meeting Companion"];
   }
 }
