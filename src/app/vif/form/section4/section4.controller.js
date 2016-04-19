e236a67b8238d12f21c6e0cf96e159e3adac5d6f
jluscomb <jluscomb@brainerd.cs.local>
2016-04-18 21:45:01 -0400

    Fixed radio button issue

--- src/app/vif/form/section4/section4.controller.js
+++ src/app/vif/form/section4/section4.controller.js
@@ -2,5 +2,8 @@ export default class VifSection4Controller {
   constructor(VifBuilder) {
     'ngInject';
     this.vif = VifBuilder;
+    this.change_hands = ["No", "Don't know", "Broker gave money to the companion", "Companion gave money to the broker"];
+    this.sect11List = ["Sexual Harassment", "Sexual Abuse", "Physical Abuse", "Threats", "Denied Proper Food", "Forced to take Drugs"];
+    this.travelList = ["Tourist Bus", "Motorbike", "Private Car", "Local Bus", "Microbus", "Plane"];
   }
 }
