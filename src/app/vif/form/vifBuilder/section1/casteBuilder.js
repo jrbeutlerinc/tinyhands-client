8393e1fe969eb33312579b7d266735a446a9fa38
Andrew Smith <videogame0201@gmail.com>
2016-04-24 22:23:50 -0400

    VIF Section 1 CasteBuilder tests

--- src/app/vif/form/vifBuilder/section1/casteBuilder.js
+++ src/app/vif/form/vifBuilder/section1/casteBuilder.js
@@ -39,7 +39,7 @@ export default class CasteBuilder {
         this._otherText = vif.victim_caste_other_value;
     }
     
-    build(vif) {
+    build(vif = {}) {
         vif.victim_caste_magar = this._magar;
         vif.victim_caste_brahmin = this._brahmin;
         vif.victim_caste_tamang = this._tamang;
@@ -53,6 +53,7 @@ export default class CasteBuilder {
         vif.victim_caste_dalit = this._dalitUnderPriviledged;
         vif.victim_caste_other = this._other;
         vif.victim_caste_other_value = this._otherText;
+        return vif;
     }
     
     get magar() {
