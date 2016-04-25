4c497e89060469bc6f0fa4954e21ea42dad3016d
amun33 <amunn33@gmail,com>
2016-04-24 20:04:56 -0400

    Added multiple choice options to section2 controller. Values should be reviewed.

--- src/app/vif/form/section2/section2.controller.js
+++ src/app/vif/form/section2/section2.controller.js
@@ -8,5 +8,59 @@ export default class VifSection2Controller {
       "Job - Broker did not say what job", "Job - Baby Care", "Job - Factory", "Job - Hotel", "Job - Shop", "Job - Laborer", "Job - Brothel", "Job - Household"];
     this.sect2Options = ["Support myself", "Support family", "Personal debt", "Family debt", "Love / Marriage", "Bad home / marriage", "Get an education",
       "Tour / Travel", "Didn't know I was going abroad"];
+    
+    this.gulfLocations = [
+        {name: "Lebanon", value: 'lebanon'},
+        {name: "Dubai", value: 'dubai'}, 
+        {name: "Saudi Arabia", value: 'saudiarabia'}, 
+        {name: "Kuwait", value: 'kuwait'}, 
+        {name: "Malaysia", value: 'malaysia'}, 
+        {name: "Oman", value: 'oman'}, 
+        {name: "Qatar", value: 'qatar'}, 
+        {name: "Did Not Know", value: 'didnotknow'}
+    ];
+    
+    this.indiaLocations = [
+        {name: "Delhi", value: 'delhi'},
+        {name: "Mumbai", value: 'mumbai'}, 
+        {name: "Surat", value: 'surat'}, 
+        {name: "Rajastan", value: 'rajastan'}, 
+        {name: "Kolkata", value: 'kolkata'}, 
+        {name: "Pune", value: 'pune'}, 
+        {name: "Jaipur", value: 'jaipur'}, 
+        {name: "Bihar", value: 'bihar'}, 
+        {name: "Did Not Know", value: 'didnotknow'}
+    ];
+    
+    this.indiaLocations = [
+        {name: "Education", value: 'education'}, 
+        {name: "Travel / Tour", value: 'traveltour'}, 
+        {name: "Shopping", value: 'shopping'}, 
+        {name: "Eloping", value: 'eloping'}, 
+        {name: "Arranged Marriage", value: 'arrangedmarriage'}, 
+        {name: "Meet your own family", value: 'meetyourownfamily'}, 
+        {name: "Visit broker's home", value: 'visitbrokershome'}, 
+        {name: "Medical treatment", value: 'medicaltreatment'}, 
+        {name: "Job - Broker did not say what job", value: 'jobbrokerdidnotsaywhatjob'}, 
+        {name: "Job - Baby Care", value: 'jobbabycare'}, 
+        {name: "Job - Factory", value: 'jobfactory'}, 
+        {name: "Job - Hotel", value: 'jobhotel'}, 
+        {name: "Job - Shop", value: 'jobshop'}, 
+        {name: "Job - Laborer", value: 'joblaborer'}, 
+        {name: "Job - Brothel", value: 'jobbrothel'}, 
+        {name: "Job - Household", value: 'jobhousehold'},
+    ];
+    
+    this.sect2Options = [
+      {name: "Support myself", value: 'supportmyself'},
+      {name: "Support family", value: 'supportfamily'},
+      {name: "Personal debt", value: 'personaldebt'},
+      {name: "Family debt", value: 'familydebt'},
+      {name: "Love / Marriage", value: 'lovemarriage'},
+      {name: "Bad home / marriage", value: 'badhomemarriage'},
+      {name: "Get an education", value: 'getaneducation'},
+      {name: "Tour / Travel", value: 'tourtravel'},
+      {name: "Didn't know I was going abroad", value: 'didntknowiwasgoingabroad'}
+    ];
   }
 }
