c2081286e35c4ea4304f8cfadb61a73be04d0a7a
dum000 <timmy_parr@yahoo.com>
2016-04-26 18:16:22 -0400

    Tested Basic Add and Delete Tab functionality.

--- src/app/vif/form/locations/locations.controller.js
+++ src/app/vif/form/locations/locations.controller.js
@@ -1,6 +1,18 @@
 export default class VifLocationsController {
   constructor(VifBuilder) {
-    'ngInject'
-    this.vif = VifBuilder;
+      'ngInject'
+      this.vif = VifBuilder;
+      this.tabs = [
+          { title:'Dynamic Title 1', content:'Dynamic content 1' },
+          { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+      ];
+      this.deleteTab = function()
+      {
+          this.tabs.pop();
+      }
+      this.addTab = function()
+      {
+          this.tabs.push({ title:'Dynamic Title 1', content:'Dynamic content 1' });
+      }
   }
 }
