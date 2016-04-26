85ca64b7721d8b63328afb66b0ea9bbc152a9bd4
amun33 <amunn33@gmail,com>
2016-04-26 14:14:01 -0400

    Finished section 4 html connecting

--- src/app/vif/form/section4/section4.controller.js
+++ src/app/vif/form/section4/section4.controller.js
@@ -2,8 +2,34 @@ export default class VifSection4Controller {
   constructor(VifBuilder) {
     'ngInject';
     this.vif = VifBuilder;
-    this.change_hands = ["No", "Don't know", "Broker gave money to the companion", "Companion gave money to the broker"];
-    this.sect11List = ["Sexual Harassment", "Sexual Abuse", "Physical Abuse", "Threats", "Denied Proper Food", "Forced to take Drugs"];
-    this.travelList = ["Tourist Bus", "Motorbike", "Private Car", "Local Bus", "Microbus", "Plane"];
+    this.change_hands = [
+        {name: "No", value: 'no'}, 
+        {name: "Don't know", value: 'dontKnow'}, 
+        {name: "Broker gave money to the companion", value: 'brokerGaveMoney'}, 
+        {name: "Companion gave money to the broker", value: 'companionGaveMoney'}
+    ];
+    this.sect11List = [
+        {name: "Sexual Harassment", value: 'sexualHarrassment'}, 
+        {name: "Sexual Abuse", value: 'sexualAbuse'}, 
+        {name: "Physical Abuse", value: 'physicalAbuse'}, 
+        {name: "Threats", value: 'threats'}, 
+        {name: "Denied Proper Food", value: 'deniedProperFood'}, 
+        {name: "Forced to take Drugs", value: 'forcedToTakeDrugs'}
+    ];
+    this.travelList = [
+        {name: "Tourist Bus", value: 'touristBus'}, 
+        {name: "Motorbike", value: 'motorbike'}, 
+        {name: "Private Car", value: 'privateCar'}, 
+        {name: "Local Bus", value: 'localBus'}, 
+        {name: "Microbus", value: 'microbus'}, 
+        {name: "Plane", value: 'plane'}
+    ];
+    this.passportList = [
+        {name: "No passport made", value: 'noPassport'},
+        {name: "Real passport made", value: 'realPassport'},
+        {name: "Passport included a false name", value: 'passportFalseName'},
+        {name: "Passport included other false info", value: 'passportFalseInfo'},
+        {name: "Passport was false", value: 'passportFalse'}
+    ];
   }
 }
