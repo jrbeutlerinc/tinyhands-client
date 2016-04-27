776d8093560c9839481b415b731f34b22e1ea11d
dum000 <timmy_parr@yahoo.com>
2016-04-26 20:54:42 -0400

    Fixed Warnings from JSHint

--- src/app/vif/form/locations/locations.controller.js
+++ src/app/vif/form/locations/locations.controller.js
@@ -6,31 +6,29 @@ export default class VifLocationsController {
       this.vif = VifBuilder;
       this.tabs = [{ id:1, content:'This is a default tab on load', deletable: false }];
 
-      this.deleteTab = function()
-      {
-          this.tabs.pop();
-      }
-
       /** Function to add a new tab **/
       this.addTab = function() {
           this.counter++;
           this.tabs.push({id:this.counter,content:'Any Content', deletable:true});
           this.selectedTab = this.tabs.length - 1; //set the newly added tab active.
-      }
+      };
+
       /** Function to delete a tab **/
       this.deleteTab = function(index) {
-          if (index != 0) {
+          if (index !== 0) {
               this.tabs.splice(index, 1); //remove the object from the array based on index
-              if (this.selectedTab == index || this.selectedTab > index) {
+              if (this.selectedTab === index || this.selectedTab > index) {
                   this.selectedTab = this.selectedTab - 1;
-                  if (this.selectedTab < 0)
+                  if (this.selectedTab < 0) {
                       this.selectedTab = 0;
+                  }
               }
           }
-      }
+      };
+
       /** Function to select a tab **/
       this.selectTab = function(index) {
           this.selectedTab = index;
-      }
+      };
   }
 }
