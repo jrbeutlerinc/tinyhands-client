e04aa184c06e978009196289f457e112ab0309c7
dum000 <timmy_parr@yahoo.com>
2016-04-26 20:29:39 -0400

    Replaced old tab code with more robust deletable tab code

--- src/app/vif/form/locations/locations.controller.js
+++ src/app/vif/form/locations/locations.controller.js
@@ -1,18 +1,30 @@
 export default class VifLocationsController {
   constructor(VifBuilder) {
-      'ngInject'
+      'ngInject';
+      this.counter = 1;
+      this.selectedTab = 0;
       this.vif = VifBuilder;
-      this.tabs = [
-          { title:'Dynamic Title 1', content:'Dynamic content 1' },
-          { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-      ];
+      this.tabs = [{ id:1, content:'This is a default tab on load', deletable: false }];
+
       this.deleteTab = function()
       {
           this.tabs.pop();
       }
-      this.addTab = function()
-      {
-          this.tabs.push({ title:'Dynamic Title 1', content:'Dynamic content 1' });
+
+      /** Function to add a new tab **/
+      this.addTab = function() {
+          this.counter++;
+          this.tabs.push({id:this.counter,content:'Any Content', deletable:true});
+          this.selectedTab = this.tabs.length - 1; //set the newly added tab active.
+      }
+      /** Function to delete a tab **/
+      this.deleteTab = function(index) {
+          if (index != 0)
+              this.tabs.splice(index, 1); //remove the object from the array based on index
+      }
+      /** Function to select a tab **/
+      this.selectTab = function(index) {
+          this.selectedTab = index;
       }
   }
 }
