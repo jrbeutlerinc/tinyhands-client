ea11a312186ec9476c0c0ebaf6e828985e18216d
Andrew Smith <videogame0201@gmail.com>
2016-04-27 21:11:26 -0400

    Minor fixes for VIF Info section
    -infoSection controller now binds to VifBuilder.infoSection
    -Changed # of Victims and # of Traffickers to input types to number

--- src/app/vif/form/info/info.controller.js
+++ src/app/vif/form/info/info.controller.js
@@ -1,7 +1,6 @@
 export default class VifInfoController {
   constructor(VifBuilder) {
     'ngInject';
-    this.vif = VifBuilder;
-    console.log(this.vif);
+    this.vif = VifBuilder.infoSection;
   }
 }
