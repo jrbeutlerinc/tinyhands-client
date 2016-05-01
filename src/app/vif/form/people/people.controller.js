04a4f531b7c0b546044f2a0a719b2249b9216c7a
johnluscombe <john_luscombe@taylor.edu>
2016-05-01 16:09:32 -0400

    Static person list

--- src/app/vif/form/people/people.controller.js
+++ src/app/vif/form/people/people.controller.js
@@ -2,12 +2,56 @@ export default class VifPeopleController {
   constructor(VifBuilder) {
     'ngInject'
     this.vif = VifBuilder;
-    this.sect1aList = ["Boss of...", "Co-worker of...", "Own relative of..."];
-    this.sect1bList = ["Broker", "Companion"];
-    this.sect1cList = ["India Trafficker", "Contact of Husband", "Known Trafficker", "Manpower", "Passport", "Sex Industry"];
-    this.sect3List = ["Kirat", "Sherpa", "Madeshi", "Pahadi", "Newari"];
-    this.occupations = ["None", "Agent (taking girls to India)", "Business owner", "Wage labor", "Job in India",
-      "Job in Gulf", "Farmer", "Teacher", "Police", "Army", "Guard", "Cook", "Driver"];
-    this.party = ["Congress", "Maoist", "UML", "Forum", "Tarai Madesh", "Shadbawona", "Raprapha Nepal Thruhat", "Nepal Janadikarik Forum", "Loktantrak Party", "Don't Know"];
+    this.sect1aList = [
+      {name: "Boss of...", value: ""},
+      {name: "Co-worker of...", value: ""},
+      {name: "Own relative of...", value: ""}
+    ];
+    this.sect1bList = [
+      {name: "Broker", value: ""},
+      {name: "Companion", value: ""}
+    ];
+    this.sect1cList = [
+      {name: "India Trafficker", value: ""},
+      {name: "Contact of Husband", value: ""},
+      {name: "Known Trafficker", value: ""},
+      {name: "Manpower", value: ""},
+      {name: "Passport", value: ""},
+      {name: "Sex Industry", value: ""}
+    ];
+    this.sect3List = [
+      {name: "Kirat", value: ""},
+      {name: "Sherpa", value: ""},
+      {name: "Madeshi", value: ""},
+      {name: "Pahadi", value: ""},
+      {name: "Newari", value: ""}
+    ];
+    this.occupations = [
+      {name: "None", value: ""},
+      {name: "Agent (taking girls to India)", value: ""},
+      {name: "Business owner", value: ""},
+      {name: "Wage labor", value: ""},
+      {name: "Job in India", value: ""},
+      {name: "Job in Gulf", value: ""},
+      {name: "Farmer", value: ""},
+      {name: "Teacher", value: ""},
+      {name: "Police", value: ""},
+      {name: "Army", value: ""},
+      {name: "Guard", value: ""},
+      {name: "Cook", value: ""},
+      {name: "Driver", value: ""}
+    ];
+    this.party = [
+      {name: "Congress", value: ""},
+      {name: "Maoist", value: ""},
+      {name: "UML", value: ""},
+      {name: "Forum", value: ""},
+      {name: "Tarai Madesh", value: ""},
+      {name: "Shadbawona", value: ""},
+      {name: "Raprapha Nepal Thruhat", value: ""},
+      {name: "Nepal Janadikarik Forum", value: ""},
+      {name: "Loktantrak Party", value: ""},
+      {name: "Don't Know", value: ""}
+    ];
   }
 }
