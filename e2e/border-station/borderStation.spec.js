e7dd29397ea5ae33048809f4e4340d9e84ed55e8
senzor <senzor@muller.cs.local>
2016-05-03 14:34:52 -0400

    standard update for push; no changes; e2e runs now with: 2 terminals; use nvm v4.2.0; protractor conf.js & webdriver manager start

--- e2e/border-station/borderStation.spec.js
+++ e2e/border-station/borderStation.spec.js
@@ -1,11 +1,20 @@
-var borderStation = require('./borderStation.po.js');
+var borderStation = require('./borderStation.po.js');\
+
 describe('borderStation view', () => {
   // browser.get('#/border-station/1');
   it("should have h1 text 'Errors'", () => {
-    
     // var borderstation = new borderStation();
     borderStation.getBorderStation();
     expect(true).toEqual(true);
   });
+  
 });
+
+
+
+
+
+
+
+
 
