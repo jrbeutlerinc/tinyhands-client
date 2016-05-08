8e2ab14e6e4d53f5ef9ff9bc528ffdaddd51e9df
dum000 <timmy_parr@yahoo.com>
2016-05-08 14:54:53 -0400

    Added CSS highlighting for VifNumber validation.

--- src/app/vif/form/info/info.controller.js
+++ src/app/vif/form/info/info.controller.js
@@ -1,12 +1,14 @@
 export default class VifInfoController {
   constructor(VifBuilder) {
-    'ngInject';
-    this.vif = VifBuilder.infoSection;
-    this.options = [
-        {name: 'AngularJS'},
-        {name: 'Bootstrap'},
-        {name: 'Foundation'},
-        {name: 'Polymer'}
-    ];
+      'ngInject';
+      this.vif = VifBuilder.infoSection;
+      this.options = [
+          {name: 'AngularJS'},
+          {name: 'Bootstrap'},
+          {name: 'Foundation'},
+          {name: 'Polymer'}
+      ];
+      this.vifNumber = "";
+      //this.station_code = document.getElementById("id_vif_number").value.slice(0,3);
   }
 }
